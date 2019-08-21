dd5f15f5f4fb93141fb43aaab811e203b3d596f7
Kalle Haavisto <kalle.haavisto@geniem.com>
2019-08-21 20:23:47 +0300

    node micro server base

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { RequestHandler, send } from 'micro';
+import { router, get } from 'microrouter';
+
+import { getHello, indexHello } from './routes';
+
+const service: RequestHandler = (req, res) => {
+  const data = {
+    status: 'ok',
+    data: {
+      message: 'Welcome to Micro',
+    },
+  };
+  send(res, 200, data);
+};
+
+module.exports = router(
+  get('/', service),
+  get('/hello', indexHello),
+  get('/hello/:toWho', getHello),
+);
