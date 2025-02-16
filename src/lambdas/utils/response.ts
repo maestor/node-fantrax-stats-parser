f90c1e5c815ab520b2a884a7743b410401deecb5
Kalle Haavisto <maestori@gmail.com>
2025-02-16 16:27:56 +0200

    Lambda response utils

---
+++ src/lambdas/utils/response.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyResult } from "aws-lambda";
+
+export const sendSuccess = <T>(body: T) => sendResponse(200, body);
+
+export const sendError = (error: string, statusCode: number = 400) =>
+  sendResponse(statusCode, { error });
+
+const sendResponse = <T>(statusCode: number, body: T): APIGatewayProxyResult => ({
+  statusCode,
+  body: JSON.stringify(body),
+});
