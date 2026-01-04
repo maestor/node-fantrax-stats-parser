7478468f007960a7080ee7b5c171ca92325689c0
Kalle Haavisto <maestori@gmail.com>
2026-01-04 15:31:14 +0200

    Add Gemini instructions, individual seasons to combined APIs

--- src/services.ts
+++ src/services.ts
@@ -29,7 +29,6 @@ const getRawDataFromFiles = async (
       return sourceToJson.map((item) => ({
         ...item,
         season,
-        isSeason: seasons.length === 1,
       }));
     } catch {
       return [];
